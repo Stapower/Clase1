697daf0583f6a7353f659a4c4c0305d7e5093459
Stapower <tomynfs2@gmail.com>
2016-08-22 21:18:43 -0300

    sigue la pràctica con el random

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <html>
 	<body>
 		<h1> Hola mundo! </h1>
+
+
 			<?php
-
-
-
-
-
 				$nombre = "Tomas";
 				echo "<h2> Hola PHP soy $nombre </h2>";
 				echo '<h2> Hola PHP soy $nombre </h2>';
 				echo '<h2> Hola PHP soy ' . $nombre . '</h2>';
 				echo '<h2> Numero random entre 10 y 100, 10 veces: </h2>';
-				$veces = 10;
+				$veces = 10000000;
 					$cont0 = 0;
 					$cont1 = 0;
 					$cont2 = 0;
@@ -28,10 +25,10 @@
 
 
 					$x = rand(0, 9);
-					echo "$x </br>";
+					//echo "$x </br>";
 					switch ($x) {
 						case '0':
-							$cont0++:
+							$cont0++;
 							break;
 
 							case '1':
@@ -65,7 +62,7 @@
 							case '8':
 							$cont8++;
 							break;
-							
+
 							case '9':
 							$cont9++;
 							break;
@@ -77,8 +74,16 @@
 					}
 				}
 
-				echo "Contadores: contador 0 : $cont0";
-
+				echo "porcentaje: de 0 : %" . ($cont0 * 100)/$veces . " es decir: $cont0 vez/es." . "</br>";
+				echo "porcentaje: de 1 : %" . ($cont1 * 100)/$veces . " es decir: $cont1 vez/es." . "</br>";
+				echo "porcentaje: de 2 : %" . ($cont2 * 100)/$veces . " es decir: $cont2 vez/es." . "</br>";
+				echo "porcentaje: de 3 : %" . ($cont3 * 100)/$veces . " es decir: $cont3 vez/es." . "</br>";
+				echo "porcentaje: de 4 : %" . ($cont4 * 100)/$veces . " es decir: $cont4 vez/es." . "</br>";
+				echo "porcentaje: de 5 : %" . ($cont5 * 100)/$veces . " es decir: $cont5 vez/es." . "</br>";
+				echo "porcentaje: de 6 : %" . ($cont6 * 100)/$veces . " es decir: $cont6 vez/es." . "</br>";
+				echo "porcentaje: de 7 : %" . ($cont7 * 100)/$veces . " es decir: $cont7 vez/es." . "</br>";
+				echo "porcentaje: de 8 : %" . ($cont8 * 100)/$veces . " es decir: $cont8 vez/es." . "</br>";
+				echo "porcentaje: de 9 : %" . ($cont9 * 100)/$veces . " es decir: $cont9 vez/es." . "</br>";
 
 			?>
 	</body>
